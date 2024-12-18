a97ea49a086779a9378dfb66bdad958aa90a3303
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2024-12-17 21:45:17 -0600

    ADD function to return variant of diff evol's class based on its name

---
+++ DifferentialEvolution/DispatchVariants.py
@@ -0,0 +1,22 @@
+from Base import DifferentialEvolution
+from RandomSample import DifferentialEvolution_RandomSample
+from Agglomerative import DifferentialEvolution_Agglomerative
+from RandomParameters import DifferentialEvolution_RandomParameters
+
+def DifferentialEvolutionVariant(Variant:str):
+    """
+        Function to select between different variants of 
+        Differential Evolution based on a string
+
+        -- Variant:str :: Variant's name
+
+        Return variant's class
+    """
+    if Variant == 'Base':
+        return DifferentialEvolution
+    elif Variant == 'RandomSample':
+        return DifferentialEvolution_RandomSample
+    elif Variant == 'Agglomerative':
+        return DifferentialEvolution_Agglomerative
+    elif Variant == 'RandomParameters':
+        return DifferentialEvolution_RandomParameters
