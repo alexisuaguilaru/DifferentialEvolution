bb815953e5f860cac309bf3b5860d818cb58951f
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2024-12-19 22:33:20 -0600

    MOD at script to get results

--- ScriptResults.py
+++ ScriptResults.py
@@ -1,19 +1,20 @@
+from multiprocess import Pool
+
 from DifferentialEvolution.DispatchVariants import DifferentialEvolutionVariant
 
-from Results.FunctionsInitProblem import ObjectiveFunctionCEC , Individual
-from Results.SimulationsCSV import SimulateOptimizer , ConvertResultsCSV
+from Results.FunctionsInitProblem import GetValidatedObjectiveFunction , ObjectiveFunctionCEC , Individual
+from Results.SimulationsCSV import SimulateOptimizer , ConvertResultsCSV , ConvertTimeExecutionCSV
 
 if __name__ == '__main__':
     yearCEC = '2017'
     dimension = 2
-    numberSimulations = 50
-    processes_jobs = 12
+    numberSimulations = 1000
+    processes_jobs = 16
 
     lowerBound , upperBound = -100 , 100
     initializeIndividual = Individual(lowerBound,upperBound,dimension)
 
     variantNames = ['Base','RandomSample','Agglomerative','RandomParameters']
-
     kwargs_Base = {
                     'FunctionEvaluations': 5000,
                     'PopulationSize': 100,
@@ -38,24 +39,33 @@ if __name__ == '__main__':
                     			'FunctionEvaluations': 5000,
                     			'PopulationSize': 100,
                   			  }
-    
     kwargs_VariantDiffEvol = {variantName:kwargs_Variant for variantName , kwargs_Variant in zip(variantNames,[kwargs_Base,kwargs_RandomSample,kwargs_Agglomerative,kwargs_RandomParameters])}
-	# ADD save time execution for each variant,function
-    for variantName in variantNames:
-        print(f'START :: {variantName}\n')
 
-        variantDiffEvol = DifferentialEvolutionVariant(variantName)
-        
-        for functionNumber in range(1,4):
-            ## ADD try - except statement
-            objectiveFunction = ObjectiveFunctionCEC(functionNumber,yearCEC,dimension)
-            optimizerDiffEvol = variantDiffEvol(objectiveFunction,initializeIndividual)
+    validatedFunctionNumbers = GetValidatedObjectiveFunction(yearCEC,dimension)
+	
+    with Pool(processes_jobs) as poolExecutions:
+        timeExecution_VariantFunctions = dict()
+        for variantName in variantNames:
+            print(f'START :: {variantName}\n')
 
-            print(f'START F{functionNumber}')
-            simulationsFunctionEvaluations , simulationsOptimals , timeExecution = SimulateOptimizer(optimizerDiffEvol,kwargs_VariantDiffEvol[variantName],numberSimulations,processes_jobs)
-            print(f'FINISH F{functionNumber}. {timeExecution}sec\n')
+            variantDiffEvol = DifferentialEvolutionVariant(variantName)
 
-            ConvertResultsCSV(variantName,simulationsFunctionEvaluations,'F',functionNumber,dimension,yearCEC)
-            ConvertResultsCSV(variantName,simulationsOptimals,'O',functionNumber,dimension,yearCEC)
+            timeExecution_Functions = []
+            for functionNumber in validatedFunctionNumbers:
+                objectiveFunction = ObjectiveFunctionCEC(functionNumber,yearCEC,dimension)
+                optimizerDiffEvol = variantDiffEvol(objectiveFunction,initializeIndividual)
 
-        print(f'END :: {variantName}\n')
+                print(f'START F{functionNumber}')
+                simulationsFunctionEvaluations , simulationsOptimals , timeExecution = SimulateOptimizer(optimizerDiffEvol,kwargs_VariantDiffEvol[variantName],numberSimulations,poolExecutions)
+                print(f'FINISH F{functionNumber}. {timeExecution}sec\n')
+
+                timeExecution_Functions.append(timeExecution)
+
+                ConvertResultsCSV(variantName,simulationsFunctionEvaluations,'F',functionNumber,dimension,yearCEC)
+                ConvertResultsCSV(variantName,simulationsOptimals,'O',functionNumber,dimension,yearCEC)
+
+            timeExecution_VariantFunctions[variantName] = timeExecution_Functions
+
+            print(f'END :: {variantName}\n')
+    
+    ConvertTimeExecutionCSV(timeExecution_VariantFunctions,validatedFunctionNumbers,dimension,yearCEC)
