d667b70d05484fac3558bdfac022a7be00c23289
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-04-12 19:08:44 -0600

    MOD at module's structure

---
+++ DifferentialEvolution/__main__.py
@@ -0,0 +1,8 @@
+from DifferentialEvolution import DifferentialEvolutionVariant
+
+if __name__ == '__main__':
+    DifferentialEvolutionVariant('Base')
+    DifferentialEvolutionVariant('FixedRandomSample')
+    DifferentialEvolutionVariant('ProportionalRandomSample')
+    DifferentialEvolutionVariant('Agglomerative')
+    DifferentialEvolutionVariant('RandomParameters')
