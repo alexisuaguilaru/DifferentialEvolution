0c24691db624c17b693ad0a0487deea3e39f8a95
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-06-11 22:55:05 -0600

    MOD at init files for fix imports of variants

--- DifferentialEvolution/Tests/test_ImportVariants.py
+++ DifferentialEvolution/Tests/test_ImportVariants.py
@@ -7,10 +7,6 @@ from DifferentialEvolution import DifferentialEvolutionVariant
     ('DifferentialEvolution_FixedRandomSample','FixedRandomSample'),
     ('DifferentialEvolution_ProportionalRandomSample','ProportionalRandomSample'),
     ('DifferentialEvolution_Agglomerative','Agglomerative'),
-    ('DifferentialEvolution_RandomParameters','RandomParameters'),
 ])
 def test_ImportVariants(VariantImport,VariantName):
     assert getattr(__import__('DifferentialEvolution.Variants',fromlist=[VariantImport]),VariantImport) == DifferentialEvolutionVariant(VariantName)
-
-if __name__ == '__main__':
-    test_ImportVariants()
